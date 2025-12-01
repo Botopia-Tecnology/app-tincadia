c93cb8bc3e20dd9b29749239b1dc295f66c6b6f4
Juan David Rincón López <juandavidrinconl@gmail.com>
2025-12-01 12:23:37 -0500

    Fix: Compiling problems of chatScreen

--- src/components/ChatsScreen.tsx
+++ src/components/ChatsScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, FlatList, Image, SafeAreaView } from 'react-native';
+import { View, Text, TouchableOpacity, FlatList, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { useTranslation } from '../hooks/useTranslation';
 import { chatsScreenStyles as styles } from '../styles/ChatsScreen.styles';
